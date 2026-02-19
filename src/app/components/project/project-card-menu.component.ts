a56e033494edab82efac6d0874d9b7e407a1ce5e
rbanning <rob@myhallpass.com>
2026-02-19 08:29:25 -0800

    update/refactor edit menu and apply to both task and project card menus

--- src/app/components/project/project-card-menu.component.ts
+++ src/app/components/project/project-card-menu.component.ts
@@ -1,18 +1,14 @@
-import { Component, computed, effect, ElementRef, inject, input, OnDestroy, output, signal } from '@angular/core';
+import { Component, computed, effect, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCircleCheck, faCircleEllipsis, faCircleEllipsisVertical, faCirclePause, faCirclePlay, faEllipsis, faEllipsisVertical, faPencilAlt, IconDefinition } from '@fortawesome/pro-duotone-svg-icons';
+import { faCircleCheck, faCircleEllipsisVertical, faCirclePause, faCirclePlay, faPencilAlt, IconDefinition } from '@fortawesome/pro-duotone-svg-icons';
 import { ProjectStatus } from '@services/project/project.model';
 import { exhaustiveCheck } from '@common/misc';
-import { Nullable } from '@common/types';
+import { ActionOption, ExpandingMenuComponent } from '@components/general/expanding-menu.component';
+import { parsers } from '@common/general';
 
 export const actions = ['edit', 'start', 'hold', 'completed'] as const;
 export type Action = typeof actions[number];
-export type ActionOption = {
-  action: Action;
-  icon: IconDefinition;
-  title: string;
-}
 
 const icons: Record<Action, IconDefinition> = {
   edit: faPencilAlt,
@@ -28,48 +24,21 @@ const allActions: Record<Action, ActionOption> = {
     completed: { action: 'completed', title: 'Mark the Project as Completed', icon: icons.completed },
 } as const;
 
-const menuStates = ['open', 'closed', 'disabled'] as const;
-type MenuState = typeof menuStates[number];
-
 @Component({
   selector: 'app-project-card-menu',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, ExpandingMenuComponent],
   template: `
-    @if (menuState() === 'closed') {
-      <button 
-        type="button"
-        title="Show Edit Menu"
-        (click)="toggleMenu('open')"
-        animate.enter="enter-fade-in-from-above"
-        animate.leave="leave-fade-out-to-above"
-        class="-translate-y-1 px-3 py-1 cursor-pointer hover:bg-slate-200"
-      >
-        <fa-duotone-icon [icon]="menuIcon" size="1x" secondaryColor="cornflowerblue" />
-      </button>
-    } 
-    @else if (menuState() === 'open') {
-      <div 
-        animate.enter="enter-fade-in-from-below"
-        animate.leave="leave-fade-out-to-below"
-        (mouseover)="resetMenuTimer()"
-        class="flex flex-col bg-white border border-slate-100 rounded" >
-        @for(item of actionList(); track item.action) {
-          <button 
-            type="button"
-            [title]="item.title"
-            (click)="handleClick(item.action)"
-            class="px-3 py-1 cursor-pointer hover:bg-slate-200"
-          >
-            <fa-duotone-icon [icon]="item.icon" size="1x" secondaryColor="cornflowerblue" />
-          </button>
-        }
-      </div>
+    <app-expanding-menu [actions]="actionList()" (activate)="handleClick($event)" />
+  `,
+  styles: `
+    :host {
+      display: block;
+      transform: translateY(-4px) translateX(4px)
     }
   `,
-  styleUrls: ['../common/enter-leave-styles.css']
 })
-export class ProjectCardMenuComponent implements OnDestroy {
+export class ProjectCardMenuComponent {
 
   //input
   originalStatus = input.required<ProjectStatus>({
@@ -82,11 +51,6 @@ export class ProjectCardMenuComponent implements OnDestroy {
   
   //protected 
   protected readonly status = signal<ProjectStatus>('pending');
-
-  protected menuState = signal<MenuState>('closed');
-  protected menuIcon = faCircleEllipsisVertical;
-  protected menuTimeout: Nullable<number> = null;
-
 
   protected readonly actionList = computed<ActionOption[]>(() => {
     const ret: ActionOption[] = [allActions.edit];  //everyone gets edit
@@ -128,40 +92,16 @@ export class ProjectCardMenuComponent implements OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    this.clearMenuTimer();
-  }
-
   //methods
 
-  protected toggleMenu(state: MenuState) {
-    this.menuState.set(state);
-    if (state === 'open') {
-      this.resetMenuTimer();
+  protected handleClick(option: ActionOption) {
+    const action = parsers.toStringUnionType(option.action, actions);
+    if (action) {
+      this.activate.emit(action);
     }
     else {
-      this.clearMenuTimer();
+      console.warn("Project Card Menu: unknown action triggered", {option});
     }
   }
 
-  protected resetMenuTimer() {
-    this.clearMenuTimer();
-    this.setMenuTimer();
-  }
-
-  protected handleClick(action: Action) {
-    this.activate.emit(action);
-  }
-
-  //private
-  private setMenuTimer() {
-    this.menuTimeout = window.setTimeout(() => {
-      this.toggleMenu('closed');
-    }, 7000);
-  }
-  private clearMenuTimer() {
-    if (this.menuTimeout) {
-      window.clearTimeout(this.menuTimeout);
-    }
-  }
 }
--- src/app/components/task/task-card.component.ts
+++ src/app/components/task/task-card.component.ts
@@ -21,38 +21,42 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   template: `
     @if (isValid()) {
     <div class="group relative p-2 pl-4 pb-4 border border-slate-300 bg-slate-50 rounded-md shadow-md overflow-hidden" [attr.data-id]="task().id">      
-      <div class="flex items-center gap-1">
-        <div class="flex-1 font-semibold text-lg leading-5 text-slate-700">{{task().name}}</div>
-        <app-task-status [status]="task().status" />
+      <div class="flex gap-1">
+        <div class="flex-1">
+          <div class="flex items-center gap-1">
+            <div class="flex-1 font-semibold text-lg leading-5 text-slate-700">{{task().name}}</div>
+            <app-task-status [status]="task().status" />
+          </div>
+          <div class="">
+            {{task().type}}
+          </div>
+          <div class="">
+            @if (task().project) {
+              <app-project-mini-card [project]="task().project!" />
+            }
+            @else {
+              <span class="text-slate-400">no project</span>
+            }
+          </div>
+          <div class="flex items-center">
+            <div class="-space-y-1 flex-1">
+              <app-task-date [date]="task().reminder" type="reminder" />
+              <app-task-date [date]="task().lastUpdated" type="updated" />
+            </div>
+            @if(!noDetails()) {
+              <button 
+                type="button"
+                title="View Details"
+                (click)="details()"
+                class="p-2 border border-slate-50 hover:bg-slate-100 rounded"
+              >
+                <fa-duotone-icon [icon]="icons.details" size="2x" [secondaryOpacity]="0.1" />
+              </button>
+            }
+          </div>
+        </div>
         @if (!readonly()) {
           <app-task-popup-editor [task]="task()" (save)="saveEdits($event)" />
-        }
-      </div>
-      <div class="">
-        {{task().type}}
-      </div>
-      <div class="">
-        @if (task().project) {
-          <app-project-mini-card [project]="task().project!" />
-        }
-        @else {
-          <span class="text-slate-400">no project</span>
-        }
-      </div>
-      <div class="flex items-center">
-        <div class="-space-y-1 flex-1">
-          <app-task-date [date]="task().reminder" type="reminder" />
-          <app-task-date [date]="task().lastUpdated" type="updated" />
-        </div>
-        @if(!noDetails()) {
-          <button 
-            type="button"
-            title="View Details"
-            (click)="details()"
-            class="p-2 border border-slate-50 hover:bg-slate-100 rounded"
-          >
-            <fa-duotone-icon [icon]="icons.details" size="2x" [secondaryOpacity]="0.1" />
-          </button>
         }
       </div>
       <app-task-status-horizontal-border [status]="task().status" />
